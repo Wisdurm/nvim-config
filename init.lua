5f56c1075622eeed2f0621884631e6543e505412
Wisdurm <luukas.kola@gmail.com>
2025-10-30 21:15:41 +0200

    oh boy...

--- init.lua
+++ init.lua
@@ -1,8 +1,21 @@
--- Tabs (tabulation) --
+-- Tabs (tabulation)
 vim.o.tabstop=4
 vim.o.shiftwidth=4
 vim.o.softtabstop=0
-vim.o.noexpandtab=true
--- Tabs (workspaces) --
+vim.o.expandtab=false
+-- Tabs (workspaces)
 vim.api.nvim_set_keymap("n", "<F6>",":tabp<CR>",{})
 vim.api.nvim_set_keymap("n", "<F7>",":tabn<CR>",{})
+-- The classics
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- Move text up
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- Move text down
+vim.keymap.set("n", "sex", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- Replace all instances of selected word
+-- Highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlights text when yanking",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+-- Plugins or whatever
--- init.vim
+++
@@ -1,7 +0,0 @@
-" Tabs (tabulation)
-set tabstop=4
-set shiftwidth=4
-set softtabstop=0 noexpandtab
-" Tabs (workspaces)
-map <F6> :tabp<CR>
-map <F7> :tabn<CR>
