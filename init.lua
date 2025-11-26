048c3f51e6dd9a34f1e51c8b8ffbcba80ff51fea
Wisdurm <luukas.kola@gmail.com>
2025-11-26 09:20:30 +0200

    skol

--- init.lua
+++ init.lua
@@ -1,8 +1,13 @@
 -- ************************************
 -- *  Actual vim/neovim config stuff  *
 -- ************************************
+-- Univeral mapping
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 -- Color
 vim.cmd('colorscheme slate')
+-- Scroll thing
+vim.opt.scrolloff = 8
 -- Folds
 vim.o.foldlevel=2
 -- Tabs (tabulation)
@@ -47,6 +52,8 @@ Plug 'nvim-lualine/lualine.nvim' -- Status bar
 Plug 'nvim-tree/nvim-web-devicons' -- Status bar icons
 Plug 'jbyuki/instant.nvim' -- Multiplayer (doesn't work)
 Plug 'xiyaowong/transparent.nvim' -- Transparent background
+-- Telescope
+Plug 'nvim-telescope/telescope.nvim' -- find files
 -- Battery plugins and dependencies
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-tree/nvim-web-devicons'
@@ -198,3 +205,41 @@ require('lualine').setup {
 -- *   multiplayer   *
 -- *******************
 vim.g.instant_username = "wisdurm"
+-- *******************
+-- *    telescope    *
+-- *******************
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = "which_key"
+      }
+    }
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  }
+}
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
