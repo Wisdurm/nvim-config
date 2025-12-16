c31c9032890f4e25ccb901f21e76e54dd6654182
Wisdurm <luukas.kola@gmail.com>
2025-12-16 12:45:14 +0200

    gdb i think i forgor

--- init.lua
+++ init.lua
@@ -75,6 +75,7 @@ Plug 'nvim-lualine/lualine.nvim' -- Status bar
 Plug 'nvim-tree/nvim-web-devicons' -- Status bar icons
 Plug 'jbyuki/instant.nvim' -- Multiplayer (doesn't work)
 Plug 'xiyaowong/transparent.nvim' -- Transparent background
+Plug 'sakhnik/nvim-gdb' -- Gdb integration
 -- Telescope
 Plug 'nvim-telescope/telescope.nvim' -- find files
 -- Battery plugins and dependencies
